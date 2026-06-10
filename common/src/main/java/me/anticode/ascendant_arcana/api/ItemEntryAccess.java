5374706497f60f7f6607191fa493e7afd835d825
Riley Rivera <rileyanticode@gmail.com>
2026-06-10 01:15:58 -0700

    Cleanup

--- common/src/main/java/me/anticode/ascendant_arcana/api/ItemEntryAccess.java
+++ common/src/main/java/me/anticode/ascendant_arcana/api/ItemEntryAccess.java
@@ -3,5 +3,5 @@ package me.anticode.ascendant_arcana.api;
 import net.minecraft.world.item.Item;
 
 public interface ItemEntryAccess {
-    net.minecraft.world.item.Item ascendantArcana$getItem();
+    Item ascendantArcana$getItem();
 }
