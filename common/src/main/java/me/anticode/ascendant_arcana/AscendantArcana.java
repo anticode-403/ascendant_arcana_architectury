e6d9f88ddf976b5fd8bc601cf10b4435f14c740d
Riley Rivera <rileyanticode@gmail.com>
2026-06-10 00:58:36 -0700

    Port server network handling

--- common/src/main/java/me/anticode/ascendant_arcana/AscendantArcana.java
+++ common/src/main/java/me/anticode/ascendant_arcana/AscendantArcana.java
@@ -1,16 +1,21 @@
 package me.anticode.ascendant_arcana;
 
+import dev.architectury.networking.NetworkManager;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import me.anticode.ascendant_arcana.config.ServerConfig;
 import me.anticode.ascendant_arcana.config.ServerConfigWrapper;
 import me.anticode.ascendant_arcana.init.*;
+import me.anticode.ascendant_arcana.networking.EnchantingScreenRemoveRecipe;
+import me.anticode.ascendant_arcana.networking.EnchantingScreenSendRecipe;
+import me.anticode.ascendant_arcana.screenhandler.AArcanaEnchantingMenu;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -37,6 +42,23 @@ public final class AscendantArcana {
         AArcanaLootFunctionTypes.initialize();
         AArcanaFeatures.initialize();
         TABS.register();
+
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, EnchantingScreenRemoveRecipe.Id, (buf, packetContext) -> {
+            Player player = packetContext.getPlayer();
+            EnchantingScreenRemoveRecipe packet = EnchantingScreenRemoveRecipe.read(buf);
+            if (player.containerMenu.containerId != packet.syncId()) return;
+            AArcanaEnchantingMenu menu = (AArcanaEnchantingMenu) player.containerMenu;
+            menu.recipe = null;
+            menu.dumpContents(true);
+        });
+
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, EnchantingScreenSendRecipe.Id, (buf, packetContext) -> {
+            Player player = packetContext.getPlayer();
+            EnchantingScreenSendRecipe packet = EnchantingScreenSendRecipe.read(buf, player.level().getRecipeManager());
+            if (player.containerMenu.containerId != packet.syncId()) return;
+            AArcanaEnchantingMenu screenHandler = (AArcanaEnchantingMenu) player.containerMenu;
+            screenHandler.recipe = packet.recipe();
+        });
     }
 
     public static void initializeConfigIfNull() {
