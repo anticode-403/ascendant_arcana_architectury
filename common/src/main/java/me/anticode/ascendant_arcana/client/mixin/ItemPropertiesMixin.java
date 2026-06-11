09db0a147d8326f13287ac54790f2ef7b88d29db
Riley Rivera <rileyanticode@gmail.com>
2026-06-10 18:57:11 -0700

    Fixed Archer's Gambit animations

--- common/src/main/java/me/anticode/ascendant_arcana/client/mixin/ItemPropertiesMixin.java
+++ common/src/main/java/me/anticode/ascendant_arcana/client/mixin/ItemPropertiesMixin.java
@@ -2,9 +2,11 @@ package me.anticode.ascendant_arcana.client.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import me.anticode.ascendant_arcana.init.AArcanaMobEffects;
 import me.anticode.ascendant_arcana.logic.RelicHelper;
 import me.anticode.ascendant_arcana.logic.Relics;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,19 +17,23 @@ import org.spongepowered.asm.mixin.injection.At;
 public class ItemPropertiesMixin {
     @ModifyReturnValue(method = "method_27890", at = @At("RETURN"))
     private static float modifyBowPullView(float original, @Local(argsOnly = true) ItemStack itemStack, @Local(argsOnly = true) LivingEntity livingEntity) {
-        return applyHasteRelic(original, itemStack);
+        return ascendant_arcana$applyHasteRelic(original, itemStack, livingEntity);
     }
 
     @ModifyReturnValue(method = "method_27888", at = @At("RETURN"))
     private static float modifyCrossbowPullView(float original, @Local(argsOnly = true) ItemStack itemStack, @Local(argsOnly = true) LivingEntity livingEntity) {
-        return applyHasteRelic(original, itemStack);
+        return ascendant_arcana$applyHasteRelic(original, itemStack, livingEntity);
     }
 
     @Unique
-    private static float applyHasteRelic(float original, @Local(argsOnly = true) ItemStack itemStack) {
+    private static float ascendant_arcana$applyHasteRelic(float original, ItemStack itemStack, LivingEntity livingEntity) {
         if (original == 0 || original == 1) return original;
-        float hasteMultiplier = 1 + (float) RelicHelper.getTooltipStrength(Relics.HASTE, RelicHelper.getValueFromNbt(itemStack.getOrCreateTag(), Relics.HASTE)) * 0.005F;
-        if (original > 1) return original;
-        return original * hasteMultiplier;
+        float hasteMultiplier = (float) RelicHelper.getTooltipStrength(Relics.HASTE, RelicHelper.getValueFromNbt(itemStack.getOrCreateTag(), Relics.HASTE)) * 0.005F;
+        float playerMultiplier = 0F;
+        MobEffectInstance effectInstance = livingEntity.getEffect(AArcanaMobEffects.ARCHERS_GAMBIT.get());
+        if (effectInstance != null) playerMultiplier = (float)(effectInstance.getAmplifier() + 1) * 0.3F;
+        float newValue = (original * (1 + playerMultiplier)) * (1 + hasteMultiplier);
+        if (newValue > 1) return 1;
+        return newValue;
     }
 }
