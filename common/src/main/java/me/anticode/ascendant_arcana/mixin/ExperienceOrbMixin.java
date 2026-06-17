b6fe394ab6a63223ca5b12b24e922e139ae5f99d
Riley Rivera <rileyanticode@gmail.com>
2026-06-17 14:08:36 -0700

    Fixed niche spawn scenarios for xp orbs

--- common/src/main/java/me/anticode/ascendant_arcana/mixin/ExperienceOrbMixin.java
+++ common/src/main/java/me/anticode/ascendant_arcana/mixin/ExperienceOrbMixin.java
@@ -10,8 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ExperienceOrb.class)
 public class ExperienceOrbMixin {
     @Inject(method = "award", at = @At("HEAD"), cancellable = true)
-    private static void discardAtTick(CallbackInfo ci) {
+    private static void discardAtAward(CallbackInfo ci) {
         if (AscendantArcana.config.disable_xp) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void discardAtTick(CallbackInfo ci) {
+        if (AscendantArcana.config.disable_xp) {
+            ((ExperienceOrb)(Object)this).discard();
             ci.cancel();
         }
     }
