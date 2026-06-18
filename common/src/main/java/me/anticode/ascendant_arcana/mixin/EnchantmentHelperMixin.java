05ae89d82b477f5292df84d83bdc3e4c8d467c32
Riley Rivera <rileyanticode@gmail.com>
2026-06-18 13:28:05 -0700

    Fixed an issue that caused meeting an item's enchantment capacity exactly to reset it.

--- common/src/main/java/me/anticode/ascendant_arcana/mixin/EnchantmentHelperMixin.java
+++ common/src/main/java/me/anticode/ascendant_arcana/mixin/EnchantmentHelperMixin.java
@@ -82,7 +82,7 @@ public class EnchantmentHelperMixin {
         int runningTotal = 0;
         for (Enchantment enchantment : value.keySet()) {
             int enchantCost = AArcanaEnchantmentHelper.getEnchantmentCost(enchantment) * value.get(enchantment);
-            if (runningTotal + enchantCost < AArcanaEnchantmentHelper.getEnchantmentCapacity(cachedStack)) {
+            if (runningTotal + enchantCost <= AArcanaEnchantmentHelper.getEnchantmentCapacity(cachedStack)) {
                 newMap.put(enchantment, value.get(enchantment));
                 runningTotal += enchantCost;
             }
