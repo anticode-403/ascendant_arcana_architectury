1d30cecb3aae44c52aae49f1329aa4f9b8b864ed
Riley Rivera <rileyanticode@gmail.com>
2026-09-20 12:38:31 -0700

    Tridents now stick outwards from Storm Anchors rather than upwards

--- common/src/main/java/me/anticode/ascendant_arcana/client/mixin/ThrownTridentRendererMixin.java
+++ common/src/main/java/me/anticode/ascendant_arcana/client/mixin/ThrownTridentRendererMixin.java
@@ -3,6 +3,7 @@ package me.anticode.ascendant_arcana.client.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import me.anticode.ascendant_arcana.api.EnchantedTrident;
+import me.anticode.ascendant_arcana.entity.LightningTurretEntity;
 import net.minecraft.client.model.TridentModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
@@ -10,12 +11,14 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.ThrownTridentRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -43,9 +46,15 @@ public abstract class ThrownTridentRendererMixin extends EntityRenderer<ThrownTr
         if (stuckEntity != null) {
             poseStack.pushPose();
             poseStack.translate(-(thrownTrident.getX() - stuckEntity.getX()), -(thrownTrident.getY() - stuckEntity.getEyeY()), -(thrownTrident.getZ() - stuckEntity.getZ()));
-            poseStack.mulPose(Axis.YP.rotationDegrees((thrownTrident.tickCount + tickDelta) * 3));
             if (stuckEntity instanceof LivingEntity) {
+                poseStack.mulPose(Axis.YP.rotationDegrees((thrownTrident.tickCount + tickDelta) * 3));
                 poseStack.mulPose(Axis.ZP.rotationDegrees(60));
+            } else if (stuckEntity instanceof LightningTurretEntity lightningTurret) {
+                Direction facing = lightningTurret.getEntityData().get(LightningTurretEntity.direction);
+                Quaternionf rotation = new Quaternionf().rotationTo(new Vector3f(0, -1, 0), new Vector3f(facing.getStepX(), facing.getStepY(), facing.getStepZ()));
+                poseStack.mulPose(rotation);
+            } else {
+                poseStack.mulPose(Axis.YP.rotationDegrees((thrownTrident.tickCount + tickDelta) * 3));
             }
             poseStack.translate(0, -enchantedTrident.ascendant_arcana$getStabTicks(), 0);
             model.renderToBuffer(poseStack, ItemRenderer.getFoilBufferDirect(multiBufferSource, model.renderType(getTextureLocation(thrownTrident)), false, thrownTrident.isFoil()), light, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
